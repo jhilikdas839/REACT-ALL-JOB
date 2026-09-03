a7e0ce86dae70b06fe52e786ac81c94aec6e1340
jhilikdas839 <jhilikdas839@gmail.com>
2026-09-03 20:14:51 +0530

    some update on Ui

--- Redux P1/src/App.jsx
+++ Redux P1/src/App.jsx
@@ -3,7 +3,9 @@ import React from 'react'
 const App = () => {
   return (
     <div>
-      App
+      <button>Increase No </button>
+      <button>Decrease No </button>
+      <h1>Hero show the Main Number </h1>
     </div>
   )
 }
